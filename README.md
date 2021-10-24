6ab2020549fb698029ec34ba8d1d14b83cb5600e
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-10-24 15:44:43 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # goit-node-hw-01
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+https://monosnap.com/file/2ybyZn910tnFeGzNfM6WxtGtYYS7RF
+
+# Получаем контакт по id
+https://monosnap.com/file/yhJWI6rPsS8pznHc1zLYYYAMwR93mQ
+
+# Добавялем контакт
+https://monosnap.com/file/VJ9IMiR3ks4AjT02OJccLmRIYGhDy3
+
+# Удаляем контакт
+https://monosnap.com/file/1oazNIjKf0RjlS5ndqslPk83n2RIEL
---
+++ contacts.js
@@ -0,0 +1,53 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.resolve("db/contacts.json");
+
+function listContacts() {
+    fs.readFile(contactsPath)
+      .then((data) => console.table(JSON.parse(data.toString())))
+      .catch((err) => console.log(err.message))
+  }
+    
+  function getContactById(contactId) {
+    fs.readFile(contactsPath)
+      .then((data) => 
+        JSON.parse(data.toString()).find(
+          (contact) => 
+            contact.id.toString() === contactId && console.table(contact)
+        )
+      )
+      .catch((err) => console.log(err.message))
+  }
+  
+  function removeContact(contactId) {
+    fs.readFile(contactsPath)
+      .then((data) => {
+        const contacts = JSON.parse(data).filter(
+          (contact) => contact.id.toString() !== contactId
+        )
+        fs.writeFile(contactsPath, JSON.stringify(contacts))
+          .then(() => 
+            console.log(`contact ${contactId} was successfully deleted`)
+          )
+          .catch((err) => console.log(err.message))
+      })
+      .catch((err) => console.log(err.message))
+  }
+  
+  function addContact(name, email, phone) {
+    fs.readFile(contactsPath)
+      .then((data) => {
+        const contacts = JSON.parse(data)
+        const id = uuidv4()
+        const newContact = { id, name, email, phone }
+        contacts.push(newContact)
+        fs.writeFile(contactsPath, JSON.stringify(contacts))
+          .then(() => console.log(`contact was successfully added`))
+          .catch((err) => console.log(err.message))
+      })
+      .catch((err) => console.log(err.message))
+  }
+  
+  module.exports = { listContacts, getContactById, removeContact, addContact }
